6274d4fed2409904a269624a4e4ff5d5b458a7ba
Shakj Shark <shakjshark@gmail.com>
2025-05-14 16:40:28 +0700

    Fix data display and API connection

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://localhost:5000/api/v1",
   headers: {
-    "Content-type": "application/json",
-  },
+    "Content-type": "application/json"
+  }
 });
