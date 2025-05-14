2d23e0ce379d3b9b7f0ba1e4ac3f131a59def914
Võ Hoàng Thảo Phương <127457929+VoHoangThaoPhuong@users.noreply.github.com>
2025-05-14 11:48:29 +0700

    Configure backend server port

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import movies from './api/movies.route.js';
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +11,11 @@ app.use(express.json());
 app.use("/api/v1/movies", movies);
 app.use('*', (req, res) => {
     res.status(404).json({ error: "Page Not Found" });
+});
+
+// Add port listening
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
 });
 
 export default app;
