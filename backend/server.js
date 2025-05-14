b9d9f8b1923b278352f7b365f20644ddb80111e9
Shakj Shark <shakjshark@gmail.com>
2025-05-14 16:39:40 +0700

    Fix web application configuration

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-import express, { json } from 'express';
+import express from 'express';
 import cors from 'cors';
 import movies from './api/movies.route.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -11,11 +10,6 @@ app.use(express.json());
 app.use("/api/v1/movies", movies);
 app.use('*', (req, res) => {
     res.status(404).json({ error: "Page Not Found" });
-});
-
-// Add port listening
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
 });
 
 export default app;
